907a38eff98ccc375f002fdd998422646ac3c73b
ss00831 <songhei_831.lee@hotmail.com>
2020-08-01 07:59:30 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -2,13 +2,17 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self): 
+    def get_friendly_name(self):
         return self.friendly_name
 
 
@@ -24,3 +28,4 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+        
