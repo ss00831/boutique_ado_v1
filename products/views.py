02e2c597b10b55d2a3846853214d3bf488d73de5
ss00831 <songhei_831.lee@hotmail.com>
2020-08-03 11:54:21 +0000

    Added sorting JS and back to top button

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -39,7 +41,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
