c8658d8eb628dd10922fcdfbb03a657f7e848722
ss00831 <songhei_831.lee@hotmail.com>
2020-08-11 08:56:27 +0000

    allow hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ss831-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ss00831-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-north-1'
